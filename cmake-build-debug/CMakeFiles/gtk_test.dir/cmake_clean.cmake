902b1751c751696e9d79a6bb85d1f54c19d7b563
jcarrara <jac1109@wildcats.unh.edu>
2018-11-19 21:45:17 -0500

    gtk clion now working

---
+++ cmake-build-debug/CMakeFiles/gtk_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtk_test.dir/main.cpp.o"
+  "gtk_test.pdb"
+  "gtk_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtk_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <gtk/gtk.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
