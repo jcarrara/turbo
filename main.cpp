7229ecf3528b16f944d4e49d2e4e488390675f27
jcarrara <jac1109@wildcats.unh.edu>
2018-11-19 22:29:48 -0500

    hello world!

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <gtk/gtk.h>
 
+static void hello( GtkWidget *widget,
+                   gpointer   data )
+{
+	g_print ("Hello World\n");
+}
+
 int main( int argc, char *argv[] ) {
-    GtkWidget *window;
 
-    gtk_init (&argc, &argv);
+	gtk_init( &argc, &argv );
 
-    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show  (window);
+	GtkWidget *window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
+	GtkWidget *button = gtk_button_new_with_label ("Hello World");
 
-    gtk_main ();
-    return 0;
+	gtk_container_add (GTK_CONTAINER (window), button);
+	gtk_widget_show (button);
+	gtk_widget_show( window );
+
+	gtk_main();
+	
+	return 0;
 }
